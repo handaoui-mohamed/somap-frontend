3568a36af5c55c21d1efd67d1201bbd90c9e91b2
handaoui-mohamed <handaoui.mohamed@gmail.com>
2017-02-23 21:40:41 +0100

    initial project layout and configuration

---
+++ src/app/index.module.js
@@ -0,0 +1,13 @@
+(function(){
+    "use strict";
+    
+    angular
+        .module("somap",[
+            "ngResource",
+            "satellizer",
+            "ngMap",
+            "ngMaterial",
+            "ngAria",
+            "ngAnimate"
+        ]);
+})();
---
+++ src/app/index.run.js
@@ -0,0 +1,11 @@
+(function(){
+    "use strict";
+
+    angular
+        .module("somap")
+        .run(run);
+
+    function run(){
+        
+    }
+})();
