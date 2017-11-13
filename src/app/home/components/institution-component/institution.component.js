57964e1c09745aaf1e397e58acb6b2d1fdeca425
handaoui-mohamed <handaoui.mohamed@gmail.com>
2017-11-13 12:12:56 +0100

    fixed selected intitution info window, used event listeners to show/hide markers

--- src/app/home/components/institution-component/institution.component.js
+++ src/app/home/components/institution-component/institution.component.js
@@ -5,37 +5,20 @@
 		.module("home")
 		.directive("institutionCard", institutionCard);
 
-	function institutionCard() {
+	function institutionCard($rootScope) {
 		return {
 			restrict: 'E',
 			replace: true,
 			templateUrl: 'app/home/components/institution-component/institution.html',
 			scope: {
-				appMap: '=',
 				institution: '=',
 				institutionClass: '=',
-				wilayas: "="
+				selected: "="
 			},
-			link: function (scope, element, attrs) {
-				scope.map = scope.appMap;
-				scope.selected = false;
-
-				scope.showOnMap = function (event, institution) {
-					var marker = scope.map.markers[institution.id];
-					if (!scope.map.markers[institution.id].getVisible()) {
-						scope.selected = true;
-						scope.map.markers[institution.id].setVisible(true);
-						var position = marker.getPosition();
-						scope.map.setCenter(new google.maps.LatLng(position.lng(), position.lat()));
-						scope.map.setZoom(13);
-						scope.map.showInfoWindow(event, 'myInfoWindow', marker);
-					} else {
-						scope.selected = false;
-						scope.map.markers[institution.id].setVisible(false);
-						scope.map.hideInfoWindow(event, 'myInfoWindow', marker);
-						scope.map.setCenter(new google.maps.LatLng(32, 2));
-						scope.map.setZoom(6);
-					}
+			link: function (scope) {
+				scope.toggle = function (event, markerId) {
+					scope.selected = !scope.selected;
+					$rootScope.$broadcast(scope.selected ? 'showInstitutionMarker' : 'hideInstitutionMarker', markerId);
 				};
 			}
 		}
--- src/app/home/components/map-component/map.component.js
+++ src/app/home/components/map-component/map.component.js
@@ -1,54 +1,67 @@
-(function() {
-    'use strict';
+(function () {
+	'use strict';
 
-    angular
-        .module("home")
-        .directive("soMap", soMap);
+	angular
+		.module("home")
+		.directive("soMap", soMap);
 
-    function soMap() {
-        return {
-            restrict: 'E',
-            replace: true,
-            templateUrl: 'app/home/components/map-component/map.html',
-            scope: {
-                selectedMarker: '=',
-                institutions: '=',
-                institutionClasses: '=',
-                atHome: "=",
-            },
-            link: function(scope, element, attrs) {
-                var goolgeMap;
-                scope.$on('mapInitialized', function(event, map) {
-                    goolgeMap = map;
-                    destroyOnMapInitilized();
-                });
+	function soMap() {
+		return {
+			restrict: 'E',
+			replace: false,
+			templateUrl: 'app/home/components/map-component/map.html',
+			scope: {
+				institutions: '=',
+				institutionClasses: '=',
+				atHome: "=",
+			},
+			link: function (scope, element, attrs) {
+				var googleMap;
+				scope.$on('mapInitialized', function (event, map) {
+					googleMap = map;
+					destroyOnMapInitilized();
+				});
 
-                function destroyOnMapInitilized(){
-                    scope.$on('mapInitialized', function(event, map) {
-                        return;
-                    });
-                }
-
- 
-                scope.onMarkerClicked = onMarkerClicked;
-                scope.onMapClicked = onMapClicked;
-
-                function onMarkerClicked(evt, marker) {
-                    goolgeMap.showInfoWindow(evt, 'myInfoWindow', goolgeMap.markers[marker.id]);
-                }
-
-                function onMapClicked(event) {
-                    // hide the infowindow if it is visible
-                    if (scope.selectedMarker != null) {
-                        goolgeMap.hideInfoWindow(event, 'myInfoWindow', scope.selectedMarker);
-                        scope.selectedMarker = null;
-                        goolgeMap.setCenter(new google.maps.LatLng(32, 2));
-                        goolgeMap.setZoom(6);
-                    } 
-                }
-            } 
-        }
-    }
+				function destroyOnMapInitilized() {
+					scope.$on('mapInitialized', function (event, map) {
+						return;
+					});
+				}
 
 
+				scope.onMarkerClicked = onMarkerClicked;
+				scope.onMapClicked = onMapClicked;
+
+				function onMarkerClicked(event, marker) {
+					var position = marker.getPosition();
+					googleMap.setCenter(new google.maps.LatLng(position.lng(), position.lat()));
+					googleMap.setZoom(13);
+					scope.selectedInstitution = scope.institutions[marker.id];
+					googleMap.showInfoWindow(event, 'myInfoWindow', googleMap.markers[marker.id]);
+				}
+
+				function onMapClicked(event) {
+					// hide the infowindow if it is visible
+					if (scope.selectedInstitution != null) {
+						googleMap.hideInfoWindow(event, 'myInfoWindow', scope.selectedInstitution);
+						scope.selectedInstitution = null;
+						googleMap.setCenter(new google.maps.LatLng(32, 2));
+						googleMap.setZoom(6);
+					}
+				}
+
+				scope.$on('showInstitutionMarker', function (event, markerId) {
+					var marker = googleMap.markers[markerId];
+					marker.setVisible(true);
+					onMarkerClicked(event, marker);
+				});
+
+				scope.$on('hideInstitutionMarker', function (event, markerId) {
+					var marker = googleMap.markers[markerId];
+					marker.setVisible(false);
+					onMapClicked(event);
+				});
+			}
+		}
+	}
 })();
