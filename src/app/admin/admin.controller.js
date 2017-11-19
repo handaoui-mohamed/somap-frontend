c2e4d07c40499c1d71faae797b1d2ff143f602f3
handaoui-mohamed <handaoui.mohamed@gmail.com>
2017-11-19 15:04:53 +0100

    added user logout

--- src/app/admin/admin.controller.js
+++ src/app/admin/admin.controller.js
@@ -5,16 +5,23 @@
 		.module("admin")
 		.controller("AdminController", AdminController);
 
-	function AdminController($mdSidenav) {
+	function AdminController($auth, $window, $state, $mdSidenav) {
 		var vm = this;
 		vm.isLockedOpen = true;
 
 		vm.toggleSideNav = toggleSideNav;
+		vm.logout = logout;
 
 		function toggleSideNav() {
 			if (!vm.isLockedOpen)
 				$mdSidenav("left").close();
 			vm.isLockedOpen = !vm.isLockedOpen;
+		}
+
+		function logout() {
+			$auth.logout();
+			$window.localStorage.removeItem('current_user');
+			$state.go('home');
 		}
 	}
 })();
