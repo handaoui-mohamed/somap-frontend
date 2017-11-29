ea0b79672a2a8707afcdd350919ab19bd41a8ca9
handaoui-mohamed <handaoui.mohamed@gmail.com>
2017-11-29 20:52:18 +0100

    added users, wilayas list

--- src/app/admin/views/users/users.controller.js
+++ src/app/admin/views/users/users.controller.js
@@ -5,13 +5,56 @@
 		.module("admin")
 		.controller("UserController", UserController);
 
-	function UserController(Toast, UserService) {
+	function UserController($mdDialog, Toast, UserService) {
 		var vm = this;
 		vm.users = [];
 
 		UserService.get(function (data) {
 			vm.users = data.elements;
-			console.log(vm.users);
 		}, function (error) { Toast.error(error) });
+
+		vm.openUserDialog = openUserDialog;
+		vm.deleteUserDialog = deleteUserDialog
+
+		function openUserDialog(event, user) {
+			$mdDialog.show({
+				controller: "UserDialogController",
+				controllerAs: 'dialVm',
+				templateUrl: "app/admin/dialogs/user-dialog/user-dialog.html",
+				parent: angular.element(document.body),
+				targetEvent: event,
+				clickOutsideToClose: true,
+				escapeToClose: true,
+				fullscreen: true,
+				locals: {
+					User: user
+				}
+			}).then(function (usr, isNew) {
+				if (isNew) {
+					vm.users.unshift(usr);
+				} else {
+					for (var key in user) {
+						if (user.hasOwnProperty(key)) {
+							user[key] = usr[key];
+						}
+					}
+				}
+			}, function () { });
+		}
+
+		function deleteUserDialog(event, userId, index) {
+			var confirm = $mdDialog.confirm()
+				.title('Suppression!')
+				.textContent('Voulez vous supprimer cet utilisateur ?')
+				.ariaLabel('validation')
+				.targetEvent(event)
+				.ok('Confirmer')
+				.cancel('Annuler');
+			$mdDialog.show(confirm).then(function () {
+				UserService.delete({ userId: userId }, function () {
+					vm.users.slice(index, 1);
+				}, function (error) { Toast.error(error) })
+			}, function () { });
+		}
 	}
 })();
--- src/app/admin/views/wilayas/wilayas.controller.js
+++ src/app/admin/views/wilayas/wilayas.controller.js
@@ -5,12 +5,57 @@
 		.module("admin")
 		.controller("WilayaController", WilayaController);
 
-	function WilayaController(Toast, WilayaService) {
+	function WilayaController($mdDialog, Toast, WilayaService) {
 		var vm = this;
 		vm.wilayas = [];
+		vm.query = { limit: 10, page: 1 };
 
 		WilayaService.get(function (data) {
 			vm.wilayas = data.elements;
 		}, function (error) { Toast.error(error) });
+
+		vm.openWilayaDialog = openWilayaDialog;
+		vm.deleteWilayaDialog = deleteWilayaDialog;
+
+		function openWilayaDialog(event, wilaya) {
+			$mdDialog.show({
+				controller: "WilayaFormDialogController",
+				controllerAs: 'dialVm',
+				templateUrl: "app/admin/dialogs/wilaya/wilaya.html",
+				parent: angular.element(document.body),
+				targetEvent: event,
+				clickOutsideToClose: true,
+				escapeToClose: true,
+				fullscreen: true,
+				locals: {
+					Wilaya: wilaya
+				}
+			}).then(function (wil, isNew) {
+				if (isNew) {
+					vm.wilayas.unshift(wil);
+				} else {
+					for (var key in wilaya) {
+						if (wilaya.hasOwnProperty(key)) {
+							wilaya[key] = wil[key];
+						}
+					}
+				}
+			}, function () { });
+		}
+
+		function deleteWilayaDialog(event, wilayaId, index) {
+			var confirm = $mdDialog.confirm()
+				.title('Suppression!')
+				.textContent('Voulez vous supprimer cette wilaya ?')
+				.ariaLabel('validation')
+				.targetEvent(event)
+				.ok('Confirmer')
+				.cancel('Annuler');
+			$mdDialog.show(confirm).then(function () {
+				WilayaService.delete({ wilayaId: wilayaId }, function () {
+					vm.wilayas.slice(index, 1);
+				}, function (error) { Toast.error(error) })
+			}, function () { });
+		}
 	}
 })();
