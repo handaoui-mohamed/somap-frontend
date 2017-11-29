8e05cc6404280821bacd3fad21a7cf9aea7a805b
handaoui-mohamed <handaoui.mohamed@gmail.com>
2017-11-29 17:38:28 +0100

    added institution classes list

--- src/app/admin/views/institution-classes/institution-classes.controller.js
+++ src/app/admin/views/institution-classes/institution-classes.controller.js
@@ -5,12 +5,57 @@
 		.module("admin")
 		.controller("InstitutionClassesController", InstitutionClassesController);
 
-	function InstitutionClassesController(Toast, InstitutionClassService) {
+	function InstitutionClassesController($mdDialog, Toast, InstitutionClassService) {
 		var vm = this;
 		vm.institutionClasses = [];
+		vm.query = { limit: 10, page: 1 };
 
 		InstitutionClassService.get(function (data) {
 			vm.institutionClasses = data.elements;
 		}, function (error) { Toast.error(error) });
+
+		vm.openInstitutionClassDialog = openInstitutionClassDialog;
+		vm.deleteInstitutionClassDialog = deleteInstitutionClassDialog;
+
+		function openInstitutionClassDialog(event, institutionClass) {
+			$mdDialog.show({
+				controller: "IntitutionClassFormDialogController",
+				controllerAs: 'dialVm',
+				templateUrl: "app/admin/dialogs/institution-class/institution-class.html",
+				parent: angular.element(document.body),
+				targetEvent: event,
+				clickOutsideToClose: true,
+				escapeToClose: true,
+				fullscreen: true,
+				locals: {
+					InstitutionClass: institutionClass
+				}
+			}).then(function (instClass, isNew) {
+				if (isNew) {
+					vm.institutionClasses.unshift(instClass);
+				} else {
+					for (var key in institutionClass) {
+						if (institutionClass.hasOwnProperty(key)) {
+							institutionClass[key] = instClass[key];
+						}
+					}
+				}
+			}, function () { });
+		}
+
+		function deleteInstitutionClassDialog(event, InstitutionClassId, index) {
+			var confirm = $mdDialog.confirm()
+				.title('Suppression!')
+				.textContent('Voulez vous supprimer ce type d\'établissement?')
+				.ariaLabel('validation')
+				.targetEvent(event)
+				.ok('Confirmer')
+				.cancel('Annuler');
+			$mdDialog.show(confirm).then(function () {
+				InstitutionClassService.delete({ InstitutionClassId: InstitutionClassId }, function () {
+					vm.InstitutionClasses.slice(index, 1);
+				}, function (error) { Toast.error(error) })
+			}, function () { });
+		}
 	}
 })();
