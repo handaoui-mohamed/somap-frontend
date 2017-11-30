9e4c1ecf1b4fedaca21e6232fd1e7cfd056a38b9
handaoui-mohamed <handaoui.mohamed@gmail.com>
2017-11-30 09:03:08 +0100

    added institution class form dialog

--- src/app/admin/dialogs/institution-class/institution-class.controller.js
+++ src/app/admin/dialogs/institution-class/institution-class.controller.js
@@ -5,5 +5,34 @@
 		.module("admin")
 		.controller("IntitutionClassDialogController", IntitutionClassDialogController);
 
-	function IntitutionClassDialogController() { }
+	function IntitutionClassDialogController($mdDialog, Toast, InstitutionClass, InstitutionClassService) {
+		var vm = this;
+		vm.institutionClass = angular.copy(InstitutionClass);
+		vm.isUpdate = !!vm.institutionClass;
+
+		vm.save = save;
+		vm.close = close;
+
+		function close() {
+			$mdDialog.hide();
+		}
+
+		function save() {
+			(vm.isUpdate ? update : add)();
+		}
+
+		function update() {
+			InstitutionClassService.save({ institutionClassId: vm.institutionClass.id }, vm.institutionClass, function (data) {
+				Toast.message("Le type d'établissement a été modifié.");
+				$mdDialog.hide(data.element);
+			}, function (error) { Toast.error(error) });
+		}
+
+		function add() {
+			InstitutionClassService.save(vm.institutionClass, function (data) {
+				Toast.message("Le type d'établissement a été ajouté.");
+				$mdDialog.hide(data.element, true);
+			}, function (error) { Toast.error(error) });
+		}
+	}
 })();
