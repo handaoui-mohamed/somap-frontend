a3892f4f57a238c8e7c8bc0705772bbdf856d06e
handaoui-mohamed <handaoui.mohamed@gmail.com>
2017-11-30 08:39:13 +0100

    added user form dialog

--- src/app/admin/dialogs/user/user.controller.js
+++ src/app/admin/dialogs/user/user.controller.js
@@ -5,5 +5,37 @@
 		.module("admin")
 		.controller("UserDialogController", UserDialogController);
 
-	function UserDialogController(){}
+	function UserDialogController($mdDialog, Toast, User, UserService, WilayaService) {
+		var vm = this;
+		vm.user = angular.copy(User);
+		vm.isUpdate = !!vm.user;
+
+		WilayaService.get(function (data) {
+			vm.wilayas = data.elements;
+		}, function (error) { Toast.error(error) });
+
+		vm.save = save;
+		vm.close = close;
+
+		function close() {
+			$mdDialog.hide();
+		}
+
+		function save() {
+			(vm.isUpdate ? update : add)();
+		}
+
+		function update() {
+			if (!vm.changePassword) delete vm.user.password;
+			UserService.save(vm.user, function (data) {
+				$mdDialog.hide(data.element);
+			}, function (error) { Toast.error(error) });
+		}
+
+		function add() {
+			UserService.save(vm.user, function (data) {
+				$mdDialog.hide(data.element, true);
+			}, function (error) { Toast.error(error) });
+		}
+	}
 })();
